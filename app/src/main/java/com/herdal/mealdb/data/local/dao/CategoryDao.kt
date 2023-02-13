f4f2a30d096eb982c60061e58143549b917773aa
herdal06 <h.erdal721@gmail.com>
2023-02-13 15:58:48 +0300

    Add CategoryDao operations

--- app/src/main/java/com/herdal/mealdb/data/local/dao/CategoryDao.kt
+++ app/src/main/java/com/herdal/mealdb/data/local/dao/CategoryDao.kt
@@ -1,7 +1,21 @@
 package com.herdal.mealdb.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.herdal.mealdb.data.local.entity.CategoryEntity
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface CategoryDao {
+
+    @Query("SELECT * FROM categories")
+    fun getAll(): Flow<List<CategoryEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(category: CategoryEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(categories: List<CategoryEntity>)
 }
