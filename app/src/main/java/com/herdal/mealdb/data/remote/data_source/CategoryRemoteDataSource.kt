f12b638fb7f901b81768e1cba94570b493eb11c7
herdal06 <h.erdal721@gmail.com>
2023-02-13 16:38:32 +0300

    Add CategoryDataSource methods

--- app/src/main/java/com/herdal/mealdb/data/remote/data_source/CategoryRemoteDataSource.kt
+++ app/src/main/java/com/herdal/mealdb/data/remote/data_source/CategoryRemoteDataSource.kt
@@ -1,10 +1,18 @@
 package com.herdal.mealdb.data.remote.data_source
 
 import com.herdal.mealdb.data.data_source.CategoryDataSource
+import com.herdal.mealdb.data.remote.dto.category.CategoryResponse
 import com.herdal.mealdb.data.remote.service.CategoryService
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class CategoryRemoteDataSource @Inject constructor(
-    private val categoryService: CategoryService
+    private val categoryService: CategoryService,
+    private val ioDispatcher: CoroutineDispatcher
 ) : CategoryDataSource.Remote {
+    override suspend fun getCategories(): CategoryResponse =
+        withContext(ioDispatcher) {
+            categoryService.getCategories()
+        }
 }
