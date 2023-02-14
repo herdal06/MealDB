5ce74dfc696fb8efe9fd22fcdec5eb422705ec5b
herdal06 <h.erdal721@gmail.com>
2023-02-14 17:05:28 +0300

    Get categories on the screen with Epoxy

--- app/src/main/java/com/herdal/mealdb/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/herdal/mealdb/presentation/home/HomeFragment.kt
@@ -5,8 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.herdal.mealdb.presentation.home.epoxy.CategoryEpoxyController
+import com.herdal.mealdb.common.Resource
 import com.herdal.mealdb.databinding.FragmentHomeBinding
+import com.herdal.mealdb.utils.ext.hide
+import com.herdal.mealdb.utils.ext.show
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
@@ -17,6 +24,12 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    private val categoryEpoxyController: CategoryEpoxyController by lazy {
+        CategoryEpoxyController()
+    }
+
+    private val viewModel: HomeViewModel by viewModels()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,11 +37,47 @@ class HomeFragment : Fragment() {
     ): View {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val view = binding.root
+        collectCategories()
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupEpoxyController()
+    }
+
+    private fun setupEpoxyController() = binding.apply {
+        rvCategories.setController(categoryEpoxyController)
+    }
+
+    private fun collectCategories() = binding.apply {
+        lifecycleScope.launchWhenStarted {
+            viewModel.getAllCategories()
+            viewModel.categories.collectLatest { resource ->
+                when (resource) {
+                    is Resource.Loading -> {
+                        tvCategoryErrorMessage.hide()
+                        rvCategories.hide()
+                    }
+                    is Resource.Success -> {
+                        tvCategoryErrorMessage.hide()
+                        pbCategory.hide()
+                        rvCategories.show()
+                        categoryEpoxyController.setData(resource.data)
+                    }
+                    is Resource.Error -> {
+                        tvCategoryErrorMessage.show()
+                        pbCategory.hide()
+                        rvCategories.hide()
+                    }
+                }
+            }
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        binding.rvCategories.adapter = null
         _binding = null
     }
 }
